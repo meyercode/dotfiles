0033b54ff82cc535faf6e27b3a591eaa155e76de
MeyerCode <adam@meyerfam.com>
2022-09-08 21:50:19 +0200

    Big update, lsp stuff

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,19 +1,59 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+local g = vim.g
+local opt = vim.opt
+local cmd = vim.cmd
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.updatetime = 50
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
 
-vim.cmd [[colorscheme gruvbox]]
-vim.opt.termguicolors = true
+opt.cursorline = true
+opt.scrolloff = 10
+opt.updatetime = 50
 
-vim.cmd [[filetype plugin on]]
+cmd [[colorscheme gruvbox]]
+opt.termguicolors = true
 
-vim.opt.clipboard = { 'unnamed', 'unnamedplus' }
+cmd [[filetype plugin on]]
 
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.swapfile = false
+opt.undofile = true
+
+opt.timeoutlen = 500
+
+local disabled_built_ins = {
+    "2html_plugin",
+    "getscript",
+    "getscriptPlugin",
+    "gzip",
+    "logipat",
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "matchit",
+    "tar",
+    "tarPlugin",
+    "rrhelper",
+    "spellfile_plugin",
+    "vimball",
+    "vimballPlugin",
+    "zip",
+    "zipPlugin",
+    "tutor",
+    "rplugin",
+    "synmenu",
+    "optwin",
+    "compiler",
+    "bugreport",
+    "ftplugin",
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    g["loaded_" .. plugin] = 1
+end
