04758cf8e3559e4cbaa77e0a00d33caf3a29e709
meyercode <adam@meyerfam.com>
2024-08-11 18:08:08 +0200

    add wezterm, might replace tmux

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,98 @@
+local wezterm = require "wezterm"
+local mux = wezterm.mux
+local act = wezterm.action
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+local config = wezterm.config_builder()
+
+local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
+config.colors = theme.colors()
+
+-- Needed for option key on SE mac keyboard
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
+config.window_background_opacity = 0.95
+
+config.window_frame = theme.window_frame()
+
+config.font = wezterm.font("JetBrains Mono")
+
+config.line_height = 1.3
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.keys = {
+  { key = "s",          mods = "LEADER|CTRL", action = act.SendKey { key = "s", mods = "CTRL" } },
+  { key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
+  { key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
+
+  -- Pane keybindings
+  { key = "s",          mods = "LEADER",      action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "v",          mods = "LEADER",      action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
+  { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
+  { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
+  { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
+  { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
+  { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
+  { key = "o",          mods = "LEADER",      action = act.RotatePanes "Clockwise" },
+
+  { key = "r",          mods = "LEADER",      action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+
+  -- Tab keybindings
+  { key = "t",          mods = "LEADER",      action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "[",          mods = "LEADER",      action = act.ActivateTabRelative(-1) },
+  { key = "]",          mods = "LEADER",      action = act.ActivateTabRelative(1) },
+  { key = "n",          mods = "LEADER",      action = act.ShowTabNavigator },
+  {
+    key = "e",
+    mods = "LEADER",
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end)
+    }
+  },
+  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+  { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+  { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+
+  -- Workspace
+  { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+
+}
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
+    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
+    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
+    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  }
+}
+
+return config
